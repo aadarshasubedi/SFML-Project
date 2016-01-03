f6456430388d56a40840a12c2500c38285b5d9c8
Blake Lead <adel.abdelhak.lille@gmail.com>
2016-01-03 18:02:38 +0100

    added Command structure

---
+++ include/Command.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Category.h"
+
+#include <SFML/System/Time.hpp>
+
+#include <functional>
+#include <cassert>
+
+class SceneNode;
+
+struct Command
+{
+	Command() : action(), category(Category::None) {}
+
+	std::function<void(SceneNode&, sf::Time)>	action;
+	unsigned int								category;
+};
+
+template <typename Object, typename Function>
+std::function<void(SceneNode&, sf::Time)> DerivedAction(Function fct)
+{
+	return [=](SceneNode & node, sf::Time dt)
+	{
+		assert(dynamic_cast<Object*>(&node) != nullptr);
+		fct(static_cast<Object&>(node), dt);
+	};
+}
